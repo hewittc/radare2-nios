dd00db68a6c2d7ae962bef1482ac66d3d010f5c4
Christopher Hewitt <1527475+hewittc@users.noreply.github.com>
2026-06-02 22:16:12 -0600

    Initial analysis function

--- plugin.c
+++ plugin.c
@@ -14,17 +14,7 @@
 #include "gnu/nios-desc.h"
 #include "gnu/nios-opc.h"
 
-#undef CGEN_OPERAND_NIL
-#define CGEN_OPERAND_NIL NIOS_OPERAND_MAX
-
-#define CGEN_CPU_OPEN_ENDIAN CGEN_CPU_OPEN_ENDIAN
-#define CGEN_ENDIAN_LITTLE CGEN_ENDIAN_LITTLE
-
-extern int nios_cgen_get_int_operand(CGEN_CPU_DESC, int, const CGEN_FIELDS *);
-extern bfd_vma nios_cgen_get_vma_operand(CGEN_CPU_DESC, int, const CGEN_FIELDS *);
-
 extern void cgen_set_parse_operand_fn(CGEN_CPU_DESC, cgen_parse_operand_fn *);
-extern const CGEN_INSN *nios_cgen_assemble_insn(CGEN_CPU_DESC, const char *, CGEN_FIELDS *, CGEN_INSN_BYTES_PTR, char **);
 
 struct nios_info {
 	CGEN_CPU_DESC cgen_cpu;
@@ -170,6 +160,345 @@ static int archinfo(RArchSession *as, ut32 q) {
 	return 2;
 }
 
+static void decode_insn(RAnalOp *op) {
+	if (!nios || !nios->cgen_cpu) {
+		return;
+	}
+
+	CGEN_CPU_DESC cd = nios->cgen_cpu;
+	CGEN_FIELDS *fields = &nios->cgen_fields;
+
+	CGEN_FIELDS_BITSIZE (fields) = 16;
+	memset (fields, 0, sizeof (*fields));
+
+	CGEN_INSN_INT insn_int = (CGEN_INSN_INT)op->bytes[1] << 8 | op->bytes[0];
+	const CGEN_INSN *insn = cgen_lookup_insn (cd, NULL, insn_int, NULL, 16, fields, 0);
+	if (!insn || CGEN_INSN_INVALID_P (insn)) {
+		return;
+	}
+
+	CGEN_EXTRACT_INFO ex_info;
+	ex_info.valid = (1 << 16) - 1;
+	ex_info.dis_info = NULL;
+	ex_info.insn_bytes = (bfd_byte *)op->bytes;
+
+	CGEN_EXTRACT_FN (cd, insn) (cd, insn, &ex_info, insn_int, fields, op->addr);
+
+	int opcode = CGEN_INSN_NUM (insn);
+	switch (opcode) {
+	case NIOS_INSN_ABS16:
+	case NIOS_INSN_ABS32:
+		op->type = R_ANAL_OP_TYPE_ABS;
+		break;
+	case NIOS_INSN_ADD16:
+	case NIOS_INSN_ADD32:
+		op->type = R_ANAL_OP_TYPE_ADD;
+		break;
+	case NIOS_INSN_ADDC16:
+		op->type = R_ANAL_OP_TYPE_ADD;
+		break;
+	case NIOS_INSN_ADDI16:
+	case NIOS_INSN_ADDI32:
+		op->type = R_ANAL_OP_TYPE_ADD;
+		break;
+	case NIOS_INSN_AND16:
+	case NIOS_INSN_AND32:
+		op->type = R_ANAL_OP_TYPE_AND;
+		break;
+	case NIOS_INSN_ANDN16:
+	case NIOS_INSN_ANDN32:
+		op->type = R_ANAL_OP_TYPE_AND;
+		break;
+	case NIOS_INSN_ASR16:
+	case NIOS_INSN_ASR32:
+		op->type = R_ANAL_OP_TYPE_SAR;
+		break;
+	case NIOS_INSN_ASRI16:
+	case NIOS_INSN_ASRI32:
+		op->type = R_ANAL_OP_TYPE_SAR;
+		break;
+	case NIOS_INSN_BGEN16:
+	case NIOS_INSN_BGEN32:
+		op->type = R_ANAL_OP_TYPE_MOV;
+		break;
+	case NIOS_INSN_BR:
+		op->type = R_ANAL_OP_TYPE_JMP;
+		op->jump = fields->f_i11_rel;
+		break;
+	case NIOS_INSN_BSR16:
+	case NIOS_INSN_BSR32:
+		op->type = R_ANAL_OP_TYPE_CALL;
+		op->jump = fields->f_i11_rel;
+		break;
+	case NIOS_INSN_BSRR16: // In CGEN but not programming guide
+		op->type = R_ANAL_OP_TYPE_UCALL;
+		op->jump = fields->f_bsrr_i6_rel;
+		break;
+	case NIOS_INSN_CALL16:
+	case NIOS_INSN_CALL32:
+		op->type = R_ANAL_OP_TYPE_RCALL;
+		op->jump = fields->f_i11_rel;
+		break;
+	case NIOS_INSN_CALLC16: // In CGEN but not programming guide
+		op->type = R_ANAL_OP_TYPE_CCALL;
+		op->jump = fields->f_i8v_rel_h;
+		op->fail = op->addr + 2;
+		break;
+	case NIOS_INSN_CALLC32: // In CGEN but not programming guide
+		op->type = R_ANAL_OP_TYPE_CCALL;
+		op->jump = fields->f_i8v_rel_w;
+		op->fail = op->addr + 2;
+		break;
+	case NIOS_INSN_CMP16:
+	case NIOS_INSN_CMP32:
+		op->type = R_ANAL_OP_TYPE_CMP;
+		break;
+	case NIOS_INSN_CMPI16:
+	case NIOS_INSN_CMPI32:
+		op->type = R_ANAL_OP_TYPE_CMP;
+		break;
+	case NIOS_INSN_EXT16D:
+	case NIOS_INSN_EXT16S:
+		op->type = R_ANAL_OP_TYPE_MOV;
+		break;
+	case NIOS_INSN_EXT8D16:
+	case NIOS_INSN_EXT8D32:
+		op->type = R_ANAL_OP_TYPE_MOV;
+		break;
+	case NIOS_INSN_EXT8S16:
+	case NIOS_INSN_EXT8S32:
+		op->type = R_ANAL_OP_TYPE_MOV;
+		break;
+	case NIOS_INSN_FILL16:
+		op->type = R_ANAL_OP_TYPE_MOV;
+		break;
+	case NIOS_INSN_FILL816:
+	case NIOS_INSN_FILL832:
+		op->type = R_ANAL_OP_TYPE_MOV;
+		break;
+	// IFS?
+	case NIOS_INSN_JMP16:
+	case NIOS_INSN_JMP32:
+		op->type = R_ANAL_OP_TYPE_RJMP;
+		break;
+	case NIOS_INSN_JMPC16: // In CGEN but not programming guide
+		op->type = R_ANAL_OP_TYPE_RJMP;
+		break;
+	case NIOS_INSN_JMPC32: // In CGEN but not programming guide
+		op->type = R_ANAL_OP_TYPE_RJMP;
+		break;
+	case NIOS_INSN_LD16:
+	case NIOS_INSN_LD32:
+		op->type = R_ANAL_OP_TYPE_LOAD;
+		break;
+	case NIOS_INSN_LDP16:
+	case NIOS_INSN_LDP32:
+		op->type = R_ANAL_OP_TYPE_LOAD;
+		break;
+	case NIOS_INSN_LDS16:
+	case NIOS_INSN_LDS32:
+		op->type = R_ANAL_OP_TYPE_LOAD;
+		break;
+	case NIOS_INSN_LSL16:
+	case NIOS_INSN_LSL32:
+		op->type = R_ANAL_OP_TYPE_SHL;
+		break;
+	case NIOS_INSN_LSLI16:
+	case NIOS_INSN_LSLI32:
+		op->type = R_ANAL_OP_TYPE_SHL;
+		break;
+	case NIOS_INSN_LSR16:
+	case NIOS_INSN_LSR32:
+		op->type = R_ANAL_OP_TYPE_SHR;
+		break;
+	case NIOS_INSN_LSRI16:
+	case NIOS_INSN_LSRI32:
+		op->type = R_ANAL_OP_TYPE_SHR;
+		break;
+	case NIOS_INSN_MOV16:
+	case NIOS_INSN_MOV32:
+		op->type = R_ANAL_OP_TYPE_MOV;
+		break;
+	case NIOS_INSN_MOVHI:
+		op->type = R_ANAL_OP_TYPE_MOV;
+		break;
+	case NIOS_INSN_MOVI16:
+	case NIOS_INSN_MOVI32:
+		op->type = R_ANAL_OP_TYPE_MOV;
+		break;
+	case NIOS_INSN_MSTEP32:
+		op->type = R_ANAL_OP_TYPE_MUL;
+		break;
+	case NIOS_INSN_MUL32:
+		op->type = R_ANAL_OP_TYPE_MUL;
+		break;
+	case NIOS_INSN_NEG16:
+	case NIOS_INSN_NEG32:
+		op->type = R_ANAL_OP_TYPE_CPL;
+		break;
+	case NIOS_INSN_NOT16:
+	case NIOS_INSN_NOT32:
+		op->type = R_ANAL_OP_TYPE_NOT;
+		break;
+	case NIOS_INSN_OR16:
+	case NIOS_INSN_OR32:
+		op->type = R_ANAL_OP_TYPE_OR;
+		break;
+	case NIOS_INSN_PFX:
+	case NIOS_INSN_PFXIO:
+		op->type = R_ANAL_OP_TYPE_MOV;
+		break;
+	case NIOS_INSN_RDCTL16:
+	case NIOS_INSN_RDCTL32:
+		op->type = R_ANAL_OP_TYPE_MOV;
+		break;
+	case NIOS_INSN_RESTORE16:
+	case NIOS_INSN_RESTORE32:
+		op->type = R_ANAL_OP_TYPE_POP;
+		break;
+	case NIOS_INSN_RLC16:
+	case NIOS_INSN_RLC32:
+		op->type = R_ANAL_OP_TYPE_ROL;
+		break;
+	case NIOS_INSN_RRC16:
+	case NIOS_INSN_RRC32:
+		op->type = R_ANAL_OP_TYPE_ROR;
+		break;
+	case NIOS_INSN_SAVE16:
+		op->stackop = R_ANAL_STACK_SET;
+		op->stackptr = (st64)fields->f_i8v * 2;
+		op->type = R_ANAL_OP_TYPE_PUSH;
+		break;
+	case NIOS_INSN_SAVE32:
+		op->stackop = R_ANAL_STACK_SET;
+		op->stackptr = (st64)fields->f_i8v * 4;
+		op->type = R_ANAL_OP_TYPE_PUSH;
+		break;
+	case NIOS_INSN_SEXT16:
+		op->type = R_ANAL_OP_TYPE_MOV;
+		break;
+	case NIOS_INSN_SEXT816:
+	case NIOS_INSN_SEXT832:
+		op->type = R_ANAL_OP_TYPE_MOV;
+		break;
+	case NIOS_INSN_SKP016:
+	case NIOS_INSN_SKP032:
+		op->type = R_ANAL_OP_TYPE_CJMP;
+		op->jump = op->addr + 4;
+		op->fail = op->addr + 2;
+		break;
+	case NIOS_INSN_SKP116:
+	case NIOS_INSN_SKP132:
+		op->type = R_ANAL_OP_TYPE_CJMP;
+		op->jump = op->addr + 4;
+		op->fail = op->addr + 2;
+		break;
+	case NIOS_INSN_SKPRNZ16:
+	case NIOS_INSN_SKPRNZ32:
+		op->type = R_ANAL_OP_TYPE_CJMP;
+		op->jump = op->addr + 4;
+		op->fail = op->addr + 2;
+		break;
+	case NIOS_INSN_SKPRZ16:
+	case NIOS_INSN_SKPRZ32:
+		op->type = R_ANAL_OP_TYPE_CJMP;
+		op->jump = op->addr + 4;
+		op->fail = op->addr + 2;
+		break;
+	case NIOS_INSN_SKPS16:
+	case NIOS_INSN_SKPS32:
+		op->type = R_ANAL_OP_TYPE_CJMP;
+		op->jump = op->addr + 4;
+		op->fail = op->addr + 2;
+		break;
+	case NIOS_INSN_ST16:
+	case NIOS_INSN_ST32:
+		op->type = R_ANAL_OP_TYPE_STORE;
+		break;
+	case NIOS_INSN_ST16D:
+	case NIOS_INSN_ST16S:
+		op->type = R_ANAL_OP_TYPE_STORE;
+		break;
+	case NIOS_INSN_ST8D16:
+	case NIOS_INSN_ST8D32:
+		op->type = R_ANAL_OP_TYPE_STORE;
+		break;
+	case NIOS_INSN_ST8S16:
+	case NIOS_INSN_ST8S32:
+		op->type = R_ANAL_OP_TYPE_STORE;
+		break;
+	case NIOS_INSN_STP16:
+	case NIOS_INSN_STP32:
+		op->type = R_ANAL_OP_TYPE_STORE;
+		break;
+	case NIOS_INSN_STS16:
+	case NIOS_INSN_STS32:
+		op->type = R_ANAL_OP_TYPE_STORE;
+		break;
+	case NIOS_INSN_STS16S:
+		op->type = R_ANAL_OP_TYPE_STORE;
+		break;
+	case NIOS_INSN_STS8S16:
+	case NIOS_INSN_STS8S32:
+		op->type = R_ANAL_OP_TYPE_STORE;
+		break;
+	case NIOS_INSN_SUB16:
+	case NIOS_INSN_SUB32:
+		op->type = R_ANAL_OP_TYPE_SUB;
+		break;
+	case NIOS_INSN_SUBC16:
+		op->type = R_ANAL_OP_TYPE_SUB;
+		break;
+	case NIOS_INSN_SUBI16:
+	case NIOS_INSN_SUBI32:
+		op->type = R_ANAL_OP_TYPE_SUB;
+		break;
+	case NIOS_INSN_SWAP32:
+		op->type = R_ANAL_OP_TYPE_MOV;
+		break;
+	case NIOS_INSN_TRAP16:
+	case NIOS_INSN_TRAP32:
+		op->type = R_ANAL_OP_TYPE_TRAP;
+		op->val = fields->f_i6v;
+		break;
+	case NIOS_INSN_TRET16:
+	case NIOS_INSN_TRET32:
+		op->type = R_ANAL_OP_TYPE_RET;
+		break;
+	case NIOS_INSN_USR016:
+	case NIOS_INSN_USR032:
+		op->type = R_ANAL_OP_TYPE_IO;
+		break;
+	case NIOS_INSN_USR116:
+	case NIOS_INSN_USR132:
+		op->type = R_ANAL_OP_TYPE_IO;
+		break;
+	case NIOS_INSN_USR216:
+	case NIOS_INSN_USR232:
+		op->type = R_ANAL_OP_TYPE_IO;
+		break;
+	case NIOS_INSN_USR316:
+	case NIOS_INSN_USR332:
+		op->type = R_ANAL_OP_TYPE_IO;
+		break;
+	case NIOS_INSN_USR416:
+	case NIOS_INSN_USR432:
+		op->type = R_ANAL_OP_TYPE_IO;
+		break;
+	case NIOS_INSN_WRCTL16:
+	case NIOS_INSN_WRCTL32:
+		op->type = R_ANAL_OP_TYPE_MOV;
+		break;
+	case NIOS_INSN_XOR16:
+	case NIOS_INSN_XOR32:
+		op->type = R_ANAL_OP_TYPE_XOR;
+		break;
+	default:
+		op->type = R_ANAL_OP_TYPE_UNK;
+		break;
+	}
+}
+
 static int nios_buffer_read_memory(bfd_vma memaddr, bfd_byte *myaddr, unsigned int length, struct disassemble_info *info) {
 	if (info && info->buffer && length <= 4) {
 		memcpy (myaddr, info->buffer, length);
@@ -210,6 +539,8 @@ static bool decode(RArchSession *as, RAnalOp *op, RArchDecodeMask mask) {
 	if (!nios || !nios->cgen_cpu) {
 		return false;
 	}
+
+	decode_insn (op);
 
 	if (mask & R_ARCH_OP_MASK_DISASM) {
 		ut8 buf[2] = { b[0], b[1] };
